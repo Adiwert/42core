c05a00b6dd2596d20c34c190c1a40a16adc7f3ba
Adiwert <stewart.ting@gmail.com>
2025-02-14 17:54:43 +0800

    ft_lstiter.c is added

---
+++ milestone0/Libft/ft_lstiter.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tstewart <tstewart@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 17:33:43 by tstewart          #+#    #+#             */
+/*   Updated: 2025/02/14 17:33:43 by tstewart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+    t_list *current;
+
+    current = lst;
+    if (lst == NULL || f == NULL)
+        return;
+    while (current != NULL)
+    {
+        f(current->content);
+        current = current->next;
+    }
+}
+
+/*
+#include <stdio.h>
+
+void uppercase(void *content)
+{
+    char *str = (char *)content;
+    while (*str)
+    {
+        *str = ft_toupper(*str);
+        str++;
+    }
+}
+
+int main(void)
+{
+    // Create linked list
+    t_list *head = ft_lstnew(strdup("hello"));
+    head->next = ft_lstnew(strdup("world"));
+
+    // Apply function to modify content
+    ft_lstiter(head, uppercase);
+
+    // Print modified list
+    printf("Modified list: ");
+    t_list *temp = head;
+    while (temp)
+    {
+        printf("%s -> ", (char *)temp->content);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+
+    // Free memory
+    ft_lstclear(&head, free);
+
+    return (0);
+}
+*/
